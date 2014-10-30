5331fe17e4a01d762f1169b9fea146de6594516a
Matthew <bennemat@seawolf.sonoma.edu>
2014-10-30 16:39:11 -0700

    Added a comment

--- hello.cpp
+++ hello.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+  // Default is "World"
   if (argc < 1)
-    {
     cout << "Hello, World" << endl;
-    }
   else
     cout << "Hello, " << argv[11] << endl;
 }
